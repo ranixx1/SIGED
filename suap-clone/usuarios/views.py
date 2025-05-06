c5a21fffb2ab5987e655117a4b49ad970c345b18
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-05-06 20:29:02 -0300

    testando o login required

--- suap-clone/usuarios/views.py
+++ suap-clone/usuarios/views.py
@@ -3,12 +3,9 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.messages import constants
 from django.contrib import messages
-from django.contrib.auth import authenticate
-from django.contrib import auth
+from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 
-
-
-# Create your views here.
+# View para cadastro de usuários
 def cadastro(request):
     if request.method == 'GET':
         return render(request, 'cadastro.html')
@@ -16,46 +13,44 @@ def cadastro(request):
         username = request.POST.get('username')
         senha = request.POST.get('senha')
         confirmar_senha = request.POST.get('confirmar_senha')
-        
-        if not senha == confirmar_senha:
-            messages.add_message(request, constants.ERROR, 'Senha e confirmar senha devem ser iguais')
+
+        # Validações básicas
+        if senha != confirmar_senha:
+            messages.error(request, 'Senha e confirmar senha devem ser iguais')
             return redirect('/usuarios/cadastro/')
-        
+
         if len(senha) < 6:
-            messages.add_message(request, constants.ERROR, 'A senha deve ter 6 ou mais caracteres')
+            messages.error(request, 'A senha deve ter 6 ou mais caracteres')
             return redirect('/usuarios/cadastro/')
-        
-        users = User.objects.filter(username=username)
-        if users.exists():
-            messages.add_message(request, constants.ERROR, 'Já existe um usuário com esse username.')
-            
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, 'Já existe um usuário com esse username.')
             return redirect('/usuarios/cadastro/')
-              
-        User.objects.create_user(
-            username=username,
-            password=senha
-        )
-       
+
+        # Criação do usuário
+        User.objects.create_user(username=username, password=senha)
+        messages.success(request, 'Usuário cadastrado com sucesso!')
         return redirect('/usuarios/login/')
-    
+
+# View para login de usuários
 def login(request):
-    
-    if request.method == "GET":
+    if request.method == 'GET':
         return render(request, 'login.html')
     elif request.method == 'POST':
-        username=request.POST.get('username')
+        username = request.POST.get('username')
         senha = request.POST.get('senha')
-       
-               
+
+        # Autenticação do usuário
         user = authenticate(request, username=username, password=senha)
-               
-        if user:
-            auth.login(request, user)
+        if user is not None:
+            auth_login(request, user)
             return redirect('/home/')
 
-        
-        messages.add_message(request, constants.ERROR, 'Username ou senhas inválidos')
-        return redirect('login')
+        messages.error(request, 'Username ou senha inválidos')
+        return redirect('/usuarios/login/')
+
+# View para logout de usuários
 def logout(request):
-    auth.logout(request)
+    auth_logout(request)
+    messages.success(request, 'Logout realizado com sucesso!')
     return redirect('/usuarios/login/')
