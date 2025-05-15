9479a69b982d835f40cb59403d24a93d180c4ae5
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-05-15 13:26:14 -0300

    add chamado a urls

--- suap-clone/App/urls.py
+++ suap-clone/App/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('cards/novo/', views.criar_card, name='criar_card'),
     path('cards/editar/<int:id>/', views.editar_card, name='editar_card'),
     path('cards/deletar/<int:id>/', views.deletar_card, name='deletar_card'),
+    path('chamados/', views.chamados, name='chamados'),
 ]
