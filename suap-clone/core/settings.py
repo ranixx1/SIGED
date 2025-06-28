80bdc181bba0a67001efe551bc27a0c20a81309b
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-06-27 21:41:17 -0300

    remove chave

--- suap-clone/core/settings.py
+++ suap-clone/core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 from django.contrib.messages import constants as messages
 from pathlib import Path, os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n0n#mby6!lv2#mg%^e&40od#8s4mb45ek42ui9jm%&b)@lm66m'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
