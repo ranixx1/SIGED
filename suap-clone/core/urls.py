a6f56859326ed0bb2144986867e8d264f449e84a
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-06-28 01:50:38 -0300

    admin (js and css)

--- suap-clone/core/urls.py
+++ suap-clone/core/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf.urls.static import static 
 from django.conf import settings
 
 
@@ -25,3 +25,6 @@ urlpatterns = [
     path('usuarios/', include('usuarios.urls')),
     path('home/', include('App.urls')),
     ] 
+if not settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
