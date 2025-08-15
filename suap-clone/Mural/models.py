9032d63f64786f70aaaf25534c7dab7f5451fe80
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-08-15 14:15:30 -0300

    feat: adicioando model do Card

--- suap-clone/Mural/models.py
+++ suap-clone/Mural/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Card(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField()
+
+    def __str__(self):
+        return self.titulo
+    
+    class Meta:
+        db_table = 'App_card'
