0af982f53205442f66badbf676ecda4291528fed
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-08-15 14:40:28 -0300

    feat: adiciona view do mural

--- suap-clone/Mural/views.py
+++ suap-clone/Mural/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+# Mural/views.py
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import JsonResponse
+from django.contrib.admin.views.decorators import staff_member_required
+from django.views.decorators.http import require_http_methods
+from .models import Card
+from .forms import CardForm 
 
-# Create your views here.
+@staff_member_required
+def criar_card(request):
+    if request.method == 'POST':
+        form = CardForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('App:home') # Redireciona para a home do app principal
+    else:
+        form = CardForm()
+    return render(request, 'Mural/criar_card.html', {'form': form})
+
+@staff_member_required
+@require_http_methods(["GET", "POST"])
+def editar_card(request, id):
+    card = get_object_or_404(Card, id=id)
+    if request.method == 'POST':
+        form = CardForm(request.POST, instance=card)
+        if form.is_valid():
+            form.save()
+            return redirect('App:home')
+    else:
+        form = CardForm(instance=card)
+    return render(request, 'Mural/editar_card.html', {'form': form, 'card': card})
+
+@staff_member_required
+@require_http_methods(["POST"])
+def deletar_card(request, id):
+    card = get_object_or_404(Card, id=id)
+    card.delete()
+    if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+        return JsonResponse({'success': True})
+    return redirect('App:home')
