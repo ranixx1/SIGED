2aa80ecaf3b7da147aceea8d1806b4b4f1bf1d93
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-08-15 02:05:24 -0300

    remove: chat consumers do asgi

--- suap-clone/core/asgi.py
+++ suap-clone/core/asgi.py
@@ -1,18 +1,8 @@
+# core/asgi.py
+
 import os
-
-
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-import App.routing # Importe o routing do seu aplicativo
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            App.routing.websocket_urlpatterns
-        )
-    ),
-})
+application = get_asgi_application()
