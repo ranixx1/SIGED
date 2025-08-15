23c9607927850c1ffc1249d07978dc9f5fda05d6
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-08-15 14:38:07 -0300

    refactore: urls da app

--- suap-clone/App/urls.py
+++ suap-clone/App/urls.py
@@ -6,14 +6,5 @@ from . import views
 
 urlpatterns = [
     path('', login_required(views.home), name='home'),
-    path('cards/novo/', views.criar_card, name='criar_card'),
-    path('cards/editar/<int:id>/', views.editar_card, name='editar_card'),
-    path('cards/deletar/<int:id>/', views.deletar_card, name='deletar_card'),
-    path('chamados/', views.chamados, name='chamados'),
-    path('chamado/criar/', views.criar_chamado, name='criar_chamado'),
-    path('meus-chamados/', views.ver_chamados, name='ver_chamados'), 
-    path('meus-chamados/<int:id>/', views.detalhe_chamado, name='detalhe_chamado'),
-
     path('dashboard-admin/', views.dashboard_admin, name='dashboard_admin'),
-    path('admin/chamados/', views.ver_chamados_admin, name='ver_chamados_admin'), # Nova rota para admin
 ]
