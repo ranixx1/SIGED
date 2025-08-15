85192ddced193df35ecd176b7351bd5d7af38226
ranilton <ranilton.c@escolar.ifrn.edu.br>
2025-08-15 14:16:40 -0300

    feat: adiciona admin Card

--- suap-clone/Mural/admin.py
+++ suap-clone/Mural/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Card
 
 # Register your models here.
+@admin.register(Card)
+class CardAdmin(admin.ModelAdmin):
+    list_display = ('id', 'titulo', 'descricao')
+    search_fields = ('titulo', 'descricao')
